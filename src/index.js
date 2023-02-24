ff58984f0084a421e578f4c53033d2a8aa921eff
ArtemTur <tur_ne@mail.ru>
2023-02-24 15:22:12 +0300

    docs:add variant

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+let another = str;
+  while (str.length > 0) {
+    str = str.replace("()", "");
+    str = str.replace("{}", "");
+    str = str.replace("[]", "");
+    str = str.replace("||", "");
+    str = str.replace("12", "");
+    str = str.replace("34", "");
+    str = str.replace("56", "");
+    str = str.replace("77", "");
+    str = str.replace("88", "");
+      if (str.length === another.length) {
+        return false;
+      } else {
+        another = str;
+      };
+    };
+    return true;
+  }
+  
+  
+ 
+
